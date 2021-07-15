95782a41e03b39c8168ded6895eb9d8edeb281bd
namubiru32 <namubirubabra2@gmail.com>
2021-07-15 13:48:39 -0700

    function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,52 +2,38 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Function to concatenate the two strings
- * @s1: The first string to concatenate
- * @s2: The second string to concatenate
- * @n: The bytes
- * Return: NULL in failure or the string concatenated
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: max no of bytes of s2 to concatenate to s1
+ * Return: if concatenation fails - NULL
+ * Otherwise, a pointer to the concatenated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *r;
-	unsigned int i = 0, j = 0, k, l = 0;
+	char *concat;
+	unsigned int len = n, index;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+
+	if (s2 == NULL)
 		s2 = "";
-	i = _strlen(s1);
-	j = _strlen(s2);
-	j = (n > j) ? j : n;
-	r = (char *)malloc((i + j) + 1);
-	if (r == NULL)
-		return (0);
-	for (k = 0; k < i; k++)
-		r[k] = s1[k];
-	for (; k < (i + j); k++)
-	{
-		r[k] = s2[l];
-		l++;
-	}
-	r[k] = '\0';
-	return (r);
-}
 
-/**
- * _strlen - funtion to get the size of an string
- * Description: A function to get the size of a
- * string given.
- * @s: The string to get size
- * Return: void
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int i = 0;
+	concat = malloc(sizeof(char) * (len + 1));
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
